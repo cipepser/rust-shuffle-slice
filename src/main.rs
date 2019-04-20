2a1220f71dbfa76d6debd079b6ccf01af78b716f
cipepser <respepic@gmail.com>
2019-04-20 18:47:26 +0900

    update to bad example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use rand::seq::SliceRandom;
+use rand::Rng;
 
 fn main() {
     let mut v = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     let mut rng = rand::thread_rng();
-    v.shuffle(&mut rng);
+    rng.shuffle(&mut v);
     println!("{:?}", v);
 }
